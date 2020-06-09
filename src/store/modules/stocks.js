376ad79fa2277f03387638624385e7f729829323
SkalletMann <37492495+SkalletMann@users.noreply.github.com>
2020-06-09 12:02:31 +0200

    removal of unnecessary quotation marks cont. modules/stocks.js

--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -5,10 +5,10 @@ const state = {
 }
 
 const mutations = {
-    'SET_STOCKS'(state, stocks) {
+    SET_STOCKS(state, stocks) {
         state.stocks = stocks;
     },
-    'RND_STOCKS' (state) {
+    RND_STOCKS(state) {
         state.stocks.forEach(stock => {
             stock.price = Math.round(stock.price * (1+ Math.random() - 0.47));
             if (stock.price == 1) stock.price = 100;
